020c931c0f6355845c4dc6d5b913997fe98bcc3c
FitzSilk <60465804+FitzSilk@users.noreply.github.com>
2020-04-03 13:55:16 +0200

    Able to make an order. Still need to check if the iD of the item exist, how to implement the Repo

--- domain/src/main/java/be/adrienhelin/project/domain/items/ItemGroup.java
+++ domain/src/main/java/be/adrienhelin/project/domain/items/ItemGroup.java
@@ -6,20 +6,24 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 
 import java.time.LocalDate;
 
 public class ItemGroup {
 
-    private final double amount;
-    private final LocalDate shippingDate;
+    private final int amount;
     @JsonDeserialize(using = StringDeserializer.class)
     @JsonSerialize(using = StringSerializer.class)
-    public final String itemId;
+    private final String itemId;
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private final LocalDate shippingDate;
 
     @JsonCreator
     public ItemGroup(@JsonProperty("itemId") String itemId,
-                     @JsonProperty("amount") double amount) {
+                     @JsonProperty("amount") int amount) {
         this.itemId = itemId;
         this.amount = amount;
         /*if((itemRepository.getItemById(itemId)).getAmount() != 0) */
@@ -31,4 +35,11 @@ public class ItemGroup {
         return itemId;
     }
 
+    public int getAmount() {
+        return amount;
+    }
+
+    public LocalDate getShippingDate() {
+        return shippingDate;
+    }
 }
