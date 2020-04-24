990bd3939ae6b94e0f07dbf28dff0dc6169ed039
FitzSilk <60465804+FitzSilk@users.noreply.github.com>
2020-04-24 11:37:51 +0200

    Fixed the bean not found: add @EntityScan and @EnableJpaRepositories.

--- jar/src/main/java/be/adrienhelin/project/application/Application.java
+++ jar/src/main/java/be/adrienhelin/project/application/Application.java
@@ -2,9 +2,12 @@ package be.adrienhelin.project.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages={"be.adrienhelin.project"})
+@EntityScan(basePackages = {"be.adrienhelin.project.domain"})
+@EnableJpaRepositories(basePackages = {"be.adrienhelin.project.domain"})
+@SpringBootApplication(scanBasePackages = {"be.adrienhelin.project"})
 public class Application {
 
     public static void main(String[] args) {
