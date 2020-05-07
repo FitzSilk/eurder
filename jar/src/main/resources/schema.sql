28fa05b2b1566d36a068180d75ee62cd5e6e1a2a
FitzSilk <60465804+FitzSilk@users.noreply.github.com>
2020-05-07 18:48:00 +0200

    Work on schema.sql and application.properties

--- jar/src/main/resources/schema.sql
+++ jar/src/main/resources/schema.sql
@@ -10,18 +10,18 @@ begin;
 -- -- END UNCOMMENT
 -- --**************
 
-create schema if not exists eeurder;
+create schema if not exists eurder;
 
-set schema 'eeurder';
+set schema 'eurder';
 
 create table if not exists item
 (
     item_id          serial        not null primary key,
     item_name        varchar(60)   not null,
-    item_description varchar(120)  not null,
+    item_description varchar(255)  not null,
     item_price       numeric(5, 2) not null,
     item_amount      int           not null,
-    item_link        varchar       not null
+    item_link        varchar(255)
 );
 
 create table if not exists customer
@@ -45,13 +45,14 @@ create table if not exists eurder
     customer_id int    not null,
     foreign key (customer_id) references customer (customer_id)
 );
+
 create table if not exists item_group
 (
-    item_group_id serial not null primary key,
-    item_id       int    not null,
-    item_amount   int    not null DEFAULT 1,
-    shipping_date date   not null,
-    eurder_id     int    not null,
+    item_group_id serial  not null primary key,
+    item_id       int     not null,
+    item_amount   int     not null DEFAULT 1,
+    shipping_date varchar not null,
+    eurder_id     int     not null,
     foreign key (eurder_id) references eurder (eurder_id),
     foreign key (item_id) references item (item_id)
 );
